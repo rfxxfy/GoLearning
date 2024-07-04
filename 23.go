31c1d239882efaff4be6240470dad6ba5322d0fd
Alina Khorst <165474281+rfxxfy@users.noreply.github.com>
2024-07-04 03:10:16 +0300

    Update 23.go

--- 23.go
+++ 23.go
@@ -1,7 +1,13 @@
-func fibonacci(n int) int {
-	switch n {
-	case 1, 2: return 1
-	default:
-		return fibonacci(n-1) + fibonacci(n-2)
+func fibonacci(n int) []int {
+	if n == 1 {
+		return []int{1}
+	} else if n == 2 {
+		return []int{1, 1}
 	}
+	ans := make([]int, n)
+	ans[0], ans[1] = 1, 1
+	for i := 2; i < n; i++ {
+		ans[i] = ans[i-1] + ans[i-2]
+	}
+	return ans
 }
