02212e4e979cbe8f63fa3032339e6e4761aa32c8
Alina Khorst <165474281+rfxxfy@users.noreply.github.com>
2024-07-04 03:03:46 +0300

    Create 22.go

---
+++ 22.go
@@ -0,0 +1,10 @@
+func bubbleSort(mass *[]int) {
+	len_ := len(*mass)
+	for i := 0; i < len_-1; i++ {
+		for j := 0; j < len_-i-1; j++ {
+			if (*mass)[j] > (*mass)[j+1] {
+				(*mass)[j], (*mass)[j+1] = (*mass)[j+1], (*mass)[j]
+			}
+		}
+	}
+}
